1b7f0925d3264624a41fa9221205f78b0ecbd272
cp-webtechnologies <nuwagabacyprian224@gmail.com>
2025-03-13 15:11:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("apply-btn").addEventListener("click", function() {
+    var emailLink = "mailto:info.goers@gmail.com?subject=Job Application&body=Please find my application attached.";
+    window.location.href = emailLink;
+});
+
